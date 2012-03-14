8e8648a887da63194635f9e725841b7183b284b9
Inti Piolanti <ipiolanti@gmail.com>
2012-03-14 02:27:50 -0300

    added ex8, aproach ex9

--- day1/scripts/script.js
+++ day1/scripts/script.js
@@ -11,6 +11,7 @@ $(document).ready(function () {
 //submit data from textbox to dispatcher.php when clicking button.
 //take the response from dispatcher and paste it into the righttext div.
 //text in righttext red when server error occurs.
+//right after the response is set inside the div, it is highlighted.
 function submitText(){
     var text=$("#alias").val();
     var url="api/dispatcher.php";
@@ -19,6 +20,26 @@ function submitText(){
                 $("#righttext p").html(data);
             })
             .error(function() { $("#righttext p").css('color','red'); })
+            .complete(function(){
+                $("#righttext p").css('background-color','green');
+            })
+}
+
+function getMovies(){
+    $.ajax({
+        url: "api/dispatcher.php",
+        data: {service: 'movie.getTop'},
+        dataType: "json",
+        success: function(data){
+            $.each(data.X, function(i,data){
+                var title= data.ShortName;
+                var year= data.ReleaseYear;
+                var syno= data.ShortSynopsis;
+                var all= title + "</br>" + year + "</br>" + syno;
+                $("#middle").html(all);
+            })
+        }
+    });
 }
 
 
