9ee7215d95132571054d2a46303be10cf6ea0d34
Inti Piolanti <ipiolanti@gmail.com>
2012-03-15 01:29:55 -0300

    day2 up to ex9

--- day2/scripts/script.js
+++ day2/scripts/script.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
 //take the response from dispatcher and paste it into the righttext div.
 //text in righttext red when server error occurs.
 //right after the response is set inside the div, it is highlighted.
+//after all previous is done, it shows a loading animation
 function submitText(){
     var text=$("#alias").val();
     var url="api/dispatcher.php";
@@ -22,24 +23,31 @@ function submitText(){
             .error(function() { $("#righttext p").css('color','red'); })
             .complete(function(){
                 $("#righttext p").css('background-color','green');
+                $.mobile.showPageLoadingMsg();
             })
 }
 
 function getMovies(){
     $.ajax({
         url: "api/dispatcher.php",
+        contentType: "application/json",
         data: {service: 'movie.getTop'},
         dataType: "json",
         success: function(data){
-            $.each(data.X, function(i,data){
-                var title= data.ShortName;
-                var year= data.ReleaseYear;
-                var syno= data.ShortSynopsis;
-                var all= title + "</br>" + year + "</br>" + syno;
-                $("#middle").html(all);
-            })
+            var JSONobject= eval(data);
+            var title= JSONobject[0].ShortName;
+            var year= JSONobject[0].ReleaseYear;
+            var syno= JSONobject[0].ShortSynopsis;
+            var img= JSONobject[0].BoxArt.SmallUrl;
+            var all= "<img src='"+img+"'/>"+title + "</br>" + year + "</br>" + syno;
+            $("#middle").html(all);
         }
     });
 }
 
-
+//finish the loading animation when ESC key is pressed
+$(document).keyup(function(e) {
+    if (e.keyCode == 27) { 
+        $.mobile.hidePageLoadingMsg(); 
+    }   
+})
